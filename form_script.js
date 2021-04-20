ada186cfc54e6a9e91e7a2a961b95909fe540418
Ramadhana Bhanuharya Wishnumurti <wishnumurti312@gmail.com~>
2021-04-20 16:47:14 +0700

    update form

--- form_script.js
+++ form_script.js
@@ -1,5 +1,5 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyxD31l8bh1uRdxkUrbXoDR8Is3WdYeBfZ_Zdym_8akSmglA4APtaTDSaHXMA37W-xFQQ/exec'
-  const form = document.forms['submit-to-google-sheet']
+const form = document.forms['submit-to-google-sheet']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
@@ -7,3 +7,4 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbyxD31l8bh1uRdxkUrbXo
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
   })
+
